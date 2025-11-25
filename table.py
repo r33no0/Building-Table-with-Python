29702f46cda21959723e295e3ba3ee93899fe005
r33no0 <polinaniazova1996@icloud.com>
2025-11-25 13:12:25 +0300

    Update table.py
    Добавлена механика заполнения строк таблицы с помощью цикла for метода add.row() из модуля PrettyTable

--- table.py
+++ table.py
@@ -2,4 +2,13 @@ from prettytable import PrettyTable
 
 receipt= PrettyTable()
 receipt.field_names = ["№" , "Продукт"  , "Цена" , "Количество" , "Стоимость"]
+
+for i in range(3):
+    product = input("Укажите название продукта : ")
+    price = int(input("Введите цену продукта"))
+    quantity = int(input("Укажите количество продукта"))
+    receipt.add_row([i+1, product , f"{price} руб." , f"{quantity}шт.", f"{price*quantity}руб." ])
+
 print(receipt)
+print(receipt)
+
