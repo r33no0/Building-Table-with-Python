9839d6be8a952df4c0692d55f662385939a9a45a
r33no0 <polinaniazova1996@icloud.com>
2025-12-02 12:36:03 +0300

    Update table.py
    Появилась возможность получить диаграмму в сопоставлении с ценой продуктов

--- table.py
+++ table.py
@@ -2,14 +2,20 @@ from prettytable import PrettyTable
 
 receipt= PrettyTable()
 receipt.field_names = ["№" , "Продукт"  , "Цена" , "Количество" , "Стоимость"]
+products = []
+pay = []
 
 for i in range(3):
     product = input("Укажите название продукта : ")
     price = int(input("Введите цену продукта"))
     quantity = int(input("Укажите количество продукта"))
     receipt.add_row([i+1, product , f"{price} руб." , f"{quantity}шт.", f"{price*quantity}руб." ])
+    products.append(product)
+    pay.append(price*quantity)
 
 print(receipt)
+diagram.bar(products, pay)
+diagram.show()
 
 
 
